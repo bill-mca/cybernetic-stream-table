e5938590e3aa8c6b78de1a5b3d2a79738657b3ea
bill-mca <w.mcalister@gmx.com>
2024-05-21 18:19:32 +1000

    Minor edits

--- ssh_commands.sh
+++ ssh_commands.sh
@@ -11,3 +11,18 @@ scp cyberneticbilby@rpi.local:~/*.jpg ~/
 # Take a high quality photo every 30 seconds for 2 minutes
 # Good for building a training dataset for machine learning
 libcamera-still -n -t 120000 --timelapse 30000 --datetime --hdr
+
+# Make GIFs to embed in 
+
+# This might be another option https://itsfoss.com/photocollage-linux/
+
+# Make a GIF from a folder of JPGs
+# You'll need to have image magick installed
+convert -delay 20 -loop 0 *.jpg myimage.gif
+
+# Get JPGs from an MP4
+ffmpeg -i video.mp4  -r 5 'frames/frame-%03d.jpg'
+
+# Run a command on a remote machine and save the output to a local file.
+ssh $REMOTE_MC "digest -a md5 $TARGET_DIR/$filename" > $HOME_DIR/remote_hash_$datetag.txt
+
